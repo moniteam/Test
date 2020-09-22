38aed606524efce2b2241767a2db834819345ea0
moniteam <chushchak89@gmail.com>
2020-09-22 22:34:02 +0300

    add any scripts

--- js/script.js
+++ js/script.js
@@ -30,30 +30,31 @@ $(document).ready(function () {
 
         $(slideList[slideNum]).addClass('form_wrapper_active');
 
+        if (slideNum > 0) {
+            $('.disabled-link').addClass('btn_back').removeClass('disabled-link');
 
-    });
-
-    if (slideNum > slideList.length) {
-        $('.disabled-link').removeClass('disabled-link').addClass('btn_back');
-        $('.disabled_btn').attr('disabled', 'nul');
-    }
-
-    $('.btn_back').click(function (e) {
-        e.preventDefault();
-
-        $(slideList[slideNum]).removeClass('form_wrapper_active');
-
-        if (slideNum < slideList.length - 2) {
-            $('.btn_get_result').text('Next Question').removeClass('btn_get_result').addClass('btn_forward');
-        } else if (slideNum == slideList.length) {
-            $(this).addClass('disabled-link');
         }
 
-        slideNum--;
+        $('.btn_back').click(function (e) {
+            e.preventDefault();
 
-        $(slideList[slideNum]).addClass('form_wrapper_active');
+            $(slideList[slideNum]).removeClass('form_wrapper_active');
 
-        console.log(slideNum);
+            if (slideNum < slideList.length - 1) {
+                $('.btn_get_result').text('Next Question').removeClass('btn_get_result').addClass('btn_forward');
+            } else if (slideNum == 0) {
+                $('.btn_back').addClass('disabled-link').removeClass('btn_back');
+            }
+
+            slideNum--;
+            $(slideList[slideNum]).addClass('form_wrapper_active');
+
+            console.log(slideNum);
+        });
     });
+
+
+
+
 
 });
