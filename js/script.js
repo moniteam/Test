260b33f9c00e92631d69eac0a616e34b2e78c0df
moniteam <chushchak89@gmail.com>
2020-09-22 22:49:45 +0300

    add any scripts AGAIN

--- js/script.js
+++ js/script.js
@@ -32,22 +32,25 @@ $(document).ready(function () {
 
         if (slideNum > 0) {
             $('.disabled-link').addClass('btn_back').removeClass('disabled-link');
-
         }
+        console.log(slideNum);
 
         $('.btn_back').click(function (e) {
             e.preventDefault();
 
             $(slideList[slideNum]).removeClass('form_wrapper_active');
 
+            slideNum--;
+            
+            $(slideList[slideNum]).addClass('form_wrapper_active');
+
             if (slideNum < slideList.length - 1) {
                 $('.btn_get_result').text('Next Question').removeClass('btn_get_result').addClass('btn_forward');
-            } else if (slideNum == 0) {
-                $('.btn_back').addClass('disabled-link').removeClass('btn_back');
             }
-
-            slideNum--;
-            $(slideList[slideNum]).addClass('form_wrapper_active');
+            if (slideNum <= 0) {
+                $('.btn_back').addClass('disabled-link').removeClass('btn_back');
+                $('.form_page_1').addClass('form_wrapper_active');
+            }
 
             console.log(slideNum);
         });
